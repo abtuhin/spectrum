8c23db9fa3c653f8a225c25664282984f82f5aea
abtuhin <tuhin9pro@gmail.com>
2023-12-10 19:25:38 +0100

    removed logs from socket

--- src/components/Websocket.js
+++ src/components/Websocket.js
@@ -7,18 +7,6 @@ const SocketComponent = ({ streamCallback = () => {}, handleActionCallback = () 
   useEffect(() => {
     const newSocket = new WebSocket(socketString);
 
-    newSocket.addEventListener('open', (event) => {
-      console.log('socket opened:', event);
-    });
-
-    newSocket.addEventListener('error', (event) => {
-      console.error('socket error:', event);
-    });
-
-    newSocket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed:', event);
-    });
-
     newSocket.addEventListener('message', (event) => {
       const data = JSON.parse(event.data);
       if (data.IsActionRequired) {
