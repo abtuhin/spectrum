a30482d2279fe11f1fe85a02bfdc0093771f15e3
abtuhin <tuhin9pro@gmail.com>
2023-12-11 18:42:45 +0100

    test updated and added comments

--- src/__tests__/hooks/useSensors.test.js
+++ src/__tests__/hooks/useSensors.test.js
@@ -1,16 +1,22 @@
 import useSensors from "@/hooks/useSensors";
 import { waitFor, renderHook } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
-
-global.fetch = jest.fn();
-
+import mockAxios from "axios";
+ 
+ 
+jest.mock("axios")
+mockAxios.get.mockResolvedValue({data: {
+  velocity: 19.65939571698243,
+  altitude: -45806.372935675514,
+  temperature: 16.73887331880602,
+  statusMessage: 'Key mission milestones achieved.',
+  isAscending: true,
+  isActionRequired: true
+}})
+ 
 describe('useSensor hook', () => {
-  beforeEach(() => {
-    global.fetch.mockResolvedValue({
-      json: jest.fn(),
-    });
-  });
-
+  afterEach(jest.clearAllMocks);
+ 
   it("should return spectrum status data", async () => {
     // GIVEN // WHEN
     const queryClient = new QueryClient();
@@ -21,16 +27,16 @@ describe('useSensor hook', () => {
         </QueryClientProvider>
       ),
     });
-
+ 
     // THEN
     await waitFor(() => {
       expect(result.current.data).not.toBeUndefined();
-      expect(result.current.data.velocity).not.toBeUndefined();
-      expect(result.current.data.altitude).not.toBeUndefined();
-      expect(result.current.data.temperature).not.toBeUndefined();
-      expect(result.current.data.statusMessage).not.toBeUndefined();
-      expect(result.current.data.isActionRequired).not.toBeUndefined();
-      expect(result.current.data.isAscending).not.toBeUndefined();
+      expect(result.current.data.velocity).toBe(19.65939571698243);
+      expect(result.current.data.altitude).toBe(-45806.372935675514);
+      expect(result.current.data.temperature).toBe(16.73887331880602);
+      expect(result.current.data.statusMessage).toBe('Key mission milestones achieved.');
+      expect(result.current.data.isActionRequired).toBe(true);
+      expect(result.current.data.isAscending).toBe(true);
     });
   });
 })
--- src/__tests__/service/socketService.test.js
+++ src/__tests__/service/socketService.test.js
@@ -16,34 +16,37 @@ describe('SocketService', () => {
 
   describe('connect', () => {
     it('should connect to WebSocket and invoke both callbacks', () => {
+      // Given
       const streamCallback = jest.fn();
       const handleActionCallback = jest.fn();
       socketService = new SocketService(streamCallback, handleActionCallback);
-
+      // When
       socketService.connect();
-
       const eventData = { IsActionRequired: true, StatusMessage: 'Test Message' };
       server.send(JSON.stringify(eventData));
-
+      // Then
       expect(streamCallback).toHaveBeenCalledWith(eventData);
       expect(handleActionCallback).toHaveBeenCalledWith('Test Message');
     });
     it('should connect to WebSocket and invoke only streamCallback', () => {
+      // Given
       const streamCallback = jest.fn();
       const handleActionCallback = jest.fn();
       socketService = new SocketService(streamCallback, handleActionCallback);
-
+      // When
       socketService.connect();
-
       const eventData = { IsActionRequired: false };
       server.send(JSON.stringify(eventData));
-
+      // Then
       expect(streamCallback).toHaveBeenCalledWith(eventData);
       expect(handleActionCallback).not.toHaveBeenCalled();
     });
     it('should close the WebSocket connection', () => {
+      // Given
       socketService.connect();
+      // When
       socketService.close();
+      // Then
       expect(server.closed).toBeTruthy();
     });
   });
